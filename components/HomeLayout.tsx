884deb9ce9f3579fac31625d165a65d499ca4a55
Yi-An Lai <yian@gem.com>
2026-07-11 23:14:46 -0700

    clean up dead code and fix a11y tree

--- components/HomeLayout.tsx
+++ components/HomeLayout.tsx
@@ -18,7 +18,7 @@ const HomeLayout = ({
         <h1 className="sr-only">Yi-An Lai - Pedal Powered Dev</h1>
         <div className="flex-col md:flex-row flex items-center md:justify-between md:h-screen">
           <div className="flex flex-col items-center xl:w-3/12 md:w-5/12 gap-2 pt-16 md:py-0">
-            <Link href={"/play"}>
+            <Link href={"/play"} aria-label="Play — Pedal Powered games">
               <Logo />
             </Link>
             <div className="text-2xl w-[280px] text-left">
